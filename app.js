831fe46ffaab9739b27c76b060e527837797c0d7
ahang1 <hang.andy.t@gmail.com>
2017-03-04 13:41:42 -0600

    js.2

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+// var searchTerm = $("#searchTerm").val();
+// console.log(searchTerm);
+
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
   'api-key': "a36f255f647b4ee88e1ae763e628f617",
-  'q': "dog"
+  // 'q': 'dog'
 });
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
-  console.log(result);
+  console.log(result.response.docs);
 }).fail(function(err) {
   throw err;
 });
+
+$(".searchButton").on("click", function(event) {
+	event.preventDefault();
+	var searchTerm = $("#searchTerm").val();
+	console.log(searchTerm);
+})
 
